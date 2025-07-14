07cba393d3a65d331294d9e8a0a5841473d244f9
Luciano Costa <lucianobragacosta031@gmail.com>
2025-07-14 17:42:34 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,18 @@
 package com.nelioalves.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.workshopmongo.domain.Post;
 
+//https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+//https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
+
+
--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,8 @@ import org.springframework.stereotype.Service;
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
-
+//https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+//https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
 @Service
 public class PostService {
 
@@ -18,6 +20,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
